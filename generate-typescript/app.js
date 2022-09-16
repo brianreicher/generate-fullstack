21bc6b73a5c186656b311474561ec81aaa35cf00
brianreicher <reicher.b@northeastern.edu>
2022-09-15 23:01:12 -0400

    TypeScript commit

---
+++ generate-typescript/app.js
@@ -0,0 +1,26 @@
+// number type - no specifics for ints or floats
+// string - 'Hi', "Hi", `Hi` 
+// boolean - true, false
+// TypeScript function
+function add(n1, n2, print, phrase) {
+    var result = n1 + n2;
+    if (print) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+// JavaScript function
+function add_js(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect input type');
+    }
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var res = "TypeScript";
+add(number1, number2, printResult, res);
+// console.log(result);
---
+++ generate-typescript/app.ts
@@ -0,0 +1,31 @@
+// number type - no specifics for ints or floats
+// string - 'Hi', "Hi", `Hi` 
+// boolean - true, false
+
+// TypeScript function
+function add(n1: number, n2: number, print: boolean, phrase: string)
+{
+    const result = n1+n2;
+    if(print){
+        console.log(phrase + result);
+    }
+    else{
+        return n1+n2;}
+}
+
+// JavaScript function
+function add_js(n1, n2)
+{
+    if(typeof n1 !== 'number' || typeof n2 !== 'number')
+    {
+        throw new Error('Incorrect input type');
+    }
+    return n1+n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const res = "TypeScript";
+add(number1, number2, printResult, res);
+// console.log(result);
