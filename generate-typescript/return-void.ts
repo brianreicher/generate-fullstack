a5baaa5f428440aba1c414729a349654001b9a27
brianreicher <reicher.b@northeastern.edu>
2022-09-16 13:32:56 -0400

    func-void

--- generate-typescript/return-void.ts
+++ generate-typescript/return-void.ts
@@ -1,5 +1,32 @@
 // Functions & types
 
-function add(n1: number, n2: number){
+function add(n1: number, n2: number): number{
     return n1+n2;
 }
+
+function printResult(num: number): void{
+    console.log('Result: ' + num);
+}
+
+printResult(add(5, 12));
+// console.log(printResult(add(5, 12))); returns VOID, even though undefined
+
+let someValue = undefined; // Functions cannot return undefined, should return void if no return statement
+
+function returnVoid(x: any): void{
+    x**2;
+}
+
+// instead of
+
+function returnUndef(x: any): undefined{
+    x**2;
+    return;
+}
+
+// Functions as types
+
+// let combinedValues: Function;
+let combinedValues: () => number;
+combinedValues = add;
+console.log(combinedValues(20, 40));
