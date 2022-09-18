2edf7d7821a5283161ec31215fc54ab56f100e03
brianreicher <reicher.b@northeastern.edu>
2022-09-18 18:34:54 -0400

    Adding ts-scratch.js

--- generate-typescript/ts-scratch.js
+++ generate-typescript/ts-scratch.js
@@ -1,4 +1,5 @@
 "use strict";
+// LOOK AT DECORATORS
 // null
 // undefined
 // boolean // true or false
@@ -27,3 +28,41 @@ var morningGlory = {
 function sayHi(greeting, name) {
     console.log(greeting + name);
 }
+// Varaiable input into strings 'Name: ${name}'
+// Rest function parameters, function return type after params with semicolon
+function singAllTheSongs(singer) {
+    var songs = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        songs[_i - 1] = arguments[_i];
+    }
+    for (var _a = 0, songs_1 = songs; _a < songs_1.length; _a++) {
+        var song_1 = songs_1[_a];
+        console.log("".concat(song_1, ", by ").concat(singer));
+    }
+}
+singAllTheSongs("Alicia Keys"); // Ok
+singAllTheSongs("Lady Gaga", "Bad Romance", "Just Dance", "Poker Face"); // Ok
+// singAllTheSongs("Ella Fitzgerald", 2000); // Not ok
+// Never & unknown return types/params
+function fail(message) {
+    throw new Error("Invariant failure: ".concat(message, "."));
+}
+function workWithUnsafeParam(param) {
+    if (typeof param !== "string") {
+        fail("param should be a string, not ".concat(typeof param));
+    }
+    // Here, param is known to be type string
+    param.toUpperCase(); // Ok
+}
+// Arrays
+// Type is a function that returns an array of strings
+var createStrings;
+// Type is an array of functions that each return a string
+var stringCreators;
+// Multidimensional Arrays
+var arrayOfArraysOfNumbers;
+arrayOfArraysOfNumbers = [
+    [1, 2, 3],
+    [2, 4, 6],
+    [3, 6, 9],
+];
